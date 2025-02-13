099c2981c828325e536307ef045a21a126995920
AlmazShai <almazhshai@gmail.com>
2025-02-13 20:06:02 +0100

    Create tests

--- test/SharedPtrTest.cpp
+++ test/SharedPtrTest.cpp
@@ -36,6 +36,12 @@ private:
     MockClass &m_mock;
 };
 
+struct TestObject
+{
+    int value;
+    explicit TestObject(int val) : value(val) {}
+};
+
 class SharedPtrTest : public ::testing::Test
 {
 protected:
@@ -74,6 +80,72 @@ TEST_F(SharedPtrTest, copy)
             EXPECT_EQ(2, ptr.getCount());
             EXPECT_EQ(2, ptr2.getCount());
         }
-        EXPECT_EQ(2, ptr.getCount());
+        EXPECT_EQ(1, ptr.getCount());
     }
+}
+
+TEST_F(SharedPtrTest, MoveConstructor)
+{
+    auto ptr1 = SharedPtr<TestObject>(new TestObject(20));
+    EXPECT_EQ(ptr1.getCount(), 1);
+
+    SharedPtr<TestObject> ptr2 = std::move(ptr1);
+    EXPECT_EQ(ptr2.getCount(), 1);
+    EXPECT_EQ(ptr1.getCount(), 0);
+}
+
+TEST_F(SharedPtrTest, CopyAssignment)
+{
+    auto ptr1 = SharedPtr<TestObject>(new TestObject(30));
+    SharedPtr<TestObject> ptr2;
+
+    ptr2 = ptr1;
+    EXPECT_EQ(ptr1.getCount(), 2);
+    EXPECT_EQ(ptr2.getCount(), 2);
+}
+
+TEST_F(SharedPtrTest, MoveAssignment)
+{
+    auto ptr1 = SharedPtr<TestObject>(new TestObject(40));
+    SharedPtr<TestObject> ptr2;
+
+    ptr2 = std::move(ptr1);
+    EXPECT_EQ(ptr2.getCount(), 1);
+    EXPECT_EQ(ptr1.getCount(), 0);
+}
+
+TEST_F(SharedPtrTest, Reset)
+{
+    auto ptr = SharedPtr<TestObject>(new TestObject(50));
+    EXPECT_EQ(ptr.getCount(), 1);
+
+    ptr.reset(new TestObject(60));
+    EXPECT_EQ(ptr.getCount(), 1);
+    EXPECT_EQ(ptr->value, 60);
+}
+
+TEST_F(SharedPtrTest, MakeShared)
+{
+    auto ptr1 = makeShared<TestObject>(70);
+    EXPECT_EQ(ptr1.getCount(), 1);
+    EXPECT_EQ(ptr1->value, 70);
+}
+
+TEST_F(SharedPtrTest, Swap)
+{
+    auto ptr1 = makeShared<TestObject>(80);
+    auto ptr2 = makeShared<TestObject>(90);
+    EXPECT_EQ(ptr1->value, 80);
+    EXPECT_EQ(ptr2->value, 90);
+
+    ptr1.swap(ptr2);
+    EXPECT_EQ(ptr1->value, 90);
+    EXPECT_EQ(ptr2->value, 80);
+}
+
+TEST_F(SharedPtrTest, Dereference)
+{
+    auto ptr = makeShared<TestObject>(100);
+    EXPECT_EQ((*ptr).value, 100);
+    EXPECT_EQ(ptr->value, 100);
 }
